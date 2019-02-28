91f782b98f028be16efb49e2f243269fa216e6f9
aterna01 <ssaniok90@mail.ru>
2019-02-28 00:09:52 +0000

    heroku test4

--- server/index.js
+++ server/index.js
@@ -49,4 +49,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () =>
+    console.log('Listening on port 3000!')
+);
